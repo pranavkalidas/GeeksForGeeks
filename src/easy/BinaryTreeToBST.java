09a7823768034a68b49981a766287a685476dc8b
pranavkalidas <65642040+pranavkalidas@users.noreply.github.com>
2023-09-08 19:52:15 +0530

    sep8

---
+++ src/easy/BinaryTreeToBST.java
@@ -0,0 +1,39 @@
+package easy;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BinaryTreeToBST {
+    Node binaryTreeToBST(Node root)
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        Collections.sort(list);
+        binaryTreeToBST(root,list);
+        return root;
+    }
+    public void inorder( Node root, ArrayList<Integer> list ){
+        if(root!=null) {
+            inorder(root.left, list);
+            list.add(root.data);
+            inorder(root.right, list);
+        }
+    }
+    public static void binaryTreeToBST( Node root, ArrayList<Integer> list ){
+        if(root!=null) {
+            binaryTreeToBST(root.left, list);
+            root.data = list.remove(0);
+            binaryTreeToBST(root.right, list);
+        }
+    }
+}
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+}
+
